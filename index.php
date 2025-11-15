2cf3f9d7b4108d013118fe475810fed0a0bd046e
soheil <soheil.com>
2025-11-15 22:01:53 +0330

    v8

--- index.php
+++ index.php
@@ -14,66 +14,9 @@
             direction: rtl;
         }
 
-        header {
-            background-color: #bb53ef;
-            color: rgb(255, 255, 255);
-            margin: 15px;
-            text-align: center;
-            padding: 20px 0;
-            font-size: 1.8rem;
-            border-radius: 20px;
-            border: 5px solid #d39af0;
-        }
-
-        .MENO {
-            border: 3px solid #d39af0;
-            margin: 7px;
-            border-radius: 10px;
-        }
-
-        .meno {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            padding: 10px;
-            gap: 20px;
-        }
-
-        .meno-home {
-            background-color: #d39af0;
-            display: inline-block;
-            border-radius: 10px;
-            width: 20%;
-        }
-
-        .meno-home:hover {
-            transform: scale(1.10);
-            transition: transform 0.3s ease;
-        }
-
-        .meno-info {
-            background-color: #d39af0;
-            display: inline-block;
-            border-radius: 10px;
-            width: 20%;
-        }
-
-        .meno-info:hover {
-            transform: scale(1.10);
-            transition: transform 0.3s ease;
-        }
-
-        .meno-login {
-            background-color: #d39af0;
-            display: inline-block;
-            border-radius: 10px;
-            width: 20%;
-        }
-
-        .meno-login:hover {
-            transform: scale(1.10);
-            transition: transform 0.3s ease;
-        }
+        <?php
+include("meno_css.php")
+        ?>
 
         .cars {
             background-color: #5a0286;
@@ -136,27 +79,9 @@
 </head>
 
 <body>
-    <header>
-        <div class="MENO">
-            MENO
-        </div>
-
-        <div class="meno">
-
-            <div class="meno-home">
-                <a href="index.html">home</a>
-            </div>
-
-            <div class="meno-info">
-                <a href="info.html">info</a>
-            </div>
-
-            <div class="meno-login">
-                <a href="login.html">login</a>
-            </div>
-
-        </div>
-    </header>
+    <?php
+include("meno_html.php")
+    ?>
 
     <div class="cars">
         ماشین ها
